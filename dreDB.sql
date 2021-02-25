3fb8933e639d61c88d5780bc46163564399a5b3d
Pwg21 <kcaskade@gmail.com>
2021-02-24 21:48:03 -0700

    created join statment returns all data

--- dreDB.sql
+++ dreDB.sql
@@ -4,23 +4,32 @@ USE dre_db;
 
 CREATE TABLE department(
   id INTEGER AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30),
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
   id INTEGER AUTO_INCREMENT NOT NULL,
-  roll VARCHAR(30),
-  salary DECIMAL(8,2),
-  department_id INTEGER
-  PRIMARY KEY (id)
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(8,2)NOT NULL,
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
   id INTEGER AUTO_INCREMENT NOT NULL,
-  firstName VARCHAR(30),
-  lastName VARCHAR(30),
-  role_id INTEGER
-  manager_id INTEGER
-  PRIMARY KEY (id)
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(30)NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
+
+-- selects all tables******
+
+-- SELECT employee.id, employee.firstName, employee.lastName, role.salary, role.title, department.name
+-- FROM employee
+-- LEFT JOIN role ON employee.role_id = role.id
+-- LEFT JOIN department ON  role.department_id = department.id;
